53dd81b8294cf88d4fa02d293935e3cf9ccb6f04
CosmicLitter <holtbd91@gmail.com>
2023-11-02 18:42:24 -0400

    Add client and session to layout and invalidate LayoutData on session mismatch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
-<script>
+<script lang="ts">
+	import { onMount } from "svelte";
   import "../app.css";
+  import type { LayoutData } from "./$types";
+	import { invalidate } from "$app/navigation";
+
+  export let data: LayoutData
+
+  $: ({ session, supabase } = data) 
+
+  onMount(() => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((event, _session) => {
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth");
+      }
+    });
+  
+    return () => subscription.unsubscribe()
+  })
 </script>
 
 <div class="m-auto flex flex-col h-screen">
