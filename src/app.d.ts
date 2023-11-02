91343c40b58df375e2d57cbdabd8db2f82429eb6
CosmicLitter <holtbd91@gmail.com>
2023-11-02 14:06:35 -0400

    Type SupabaseClient with Database structure

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
-
-import type { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "$lib/supabase-types";
+import type { Session, SupabaseClient } from "@supabase/supabase-js";
 
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			supabase: SupabaseClient
+			supabase: SupabaseClient<Database>
 			getSession(): Promise<Session | null>
 		}
 		interface PageData {
